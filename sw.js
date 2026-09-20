030186eef1cc99994838cdf3c5fc63cf57103f57
singhabhi0716-alt <singhabhi0716@gmail.com>
2026-09-20 14:55:40 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,59 @@
-const CACHE="my-finance-calculators-v4";
-const ASSETS=["./","./index.html","./manifest.webmanifest","./sw.js","./icon-192.png","./icon-512.png"];
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
-self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE="my-finance-calculators-v6";
+
+const ASSETS=[
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./sw.js",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install",event=>{
+  event.waitUntil(
+    caches.open(CACHE)
+      .then(cache=>cache.addAll(ASSETS))
+      .then(()=>self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate",event=>{
+  event.waitUntil(
+    caches.keys()
+      .then(keys =>
+        Promise.all(
+          keys
+            .filter(key=>key!==CACHE)
+            .map(key=>caches.delete(key))
+        )
+      )
+      .then(()=>self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch",event=>{
+  const url=new URL(event.request.url);
+
+  if(
+    url.pathname.endsWith("/index.html") ||
+    url.pathname.endsWith("/sw.js") ||
+    url.pathname.endsWith("/manifest.webmanifest")
+  ){
+    event.respondWith(
+      fetch(event.request)
+        .then(response=>{
+          const copy=response.clone();
+          caches.open(CACHE).then(cache=>{
+            cache.put(event.request,copy);
+          });
+          return response;
+        })
+        .catch(()=>caches.match(event.request))
+    );
+  }else{
+    event.respondWith(
+      caches.match(event.request)
+        .then(response=>response || fetch(event.request))
+    );
+  }
+});
